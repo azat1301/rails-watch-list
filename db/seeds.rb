95949122371084a3a58bfb2521b47a34efb96a7c
Azat Allayarov <azatallayarow@gmail.com>
2022-05-12 21:59:29 +0900

    day1 final submit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+Movie.destroy_all
+# List.destroy_all
+
+# the Le Wagon copy of the API
+url = 'http://tmdb.lewagon.com/movie/top_rated'
+response = JSON.parse(URI.open(url).read)
+puts "Creating movies...."
+response['results'].each do |movie_hash|
+  # create an instance with the hash
+  Movie.create!(
+    title: movie_hash['title'],
+    overview: movie_hash['overview'],
+    poster_url: "https://image.tmdb.org/t/p/w500" + movie_hash['poster_path'],
+    rating: movie_hash['vote_average']
+  )
+end
+puts "Created #{Movie.count} movies."
